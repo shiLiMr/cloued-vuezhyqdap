266538a80b8dcba1a7628426424d8187197983f3
shilihaoa <lhxhl0303.163.com>
2024-07-07 19:50:05 +0800

    微前端：乾坤

--- src/main.js
+++ src/main.js
@@ -8,9 +8,38 @@ import router from './router'
 
 import 'default-passive-events'
 
-const app = createApp(App)
+import { renderWithQiankun, qiankunWindow } from 'vite-plugin-qiankun/dist/helper'
 
-app.use(createPinia())
-app.use(router)
 
-app.mount('#app')
+// 使用乾坤渲染
+renderWithQiankun({
+    // 挂载时
+    mount (props) {
+        console.log('mount');
+        render(props)
+    },
+    bootstrap(){
+
+        console.log('bootstrap');
+    },
+    // 卸载时
+    unmount (props) {
+        console.log('unmount', props);
+    }
+})
+
+//若独立运行时
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+    render({})
+}
+// rander函数
+function render (props = {}){
+    const { container } = props
+    console.log('container', container);
+    const app = createApp(App)
+    app.use(createPinia())
+    app.use(router)
+
+
+    app.mount(container ? container.querySelector('#app') : '#app')
+}
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import qiankun from 'vite-plugin-qiankun'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    qiankun('vuezhyqdap',{
+        useDevMode: true,// 开发模式
+    })
   ],
   resolve: {
     alias: {
@@ -14,15 +19,17 @@ export default defineConfig({
     }
   },
   server:{
-    // port: 5173,
+    port: 8888,
     //域名
-    host: 'localhost',
+    // host: 'localhost',
     proxy:{
       '/api': {
         target: 'https://api-hmzs.itheima.net/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
-    }
+    },
+    // 防止开发阶段的assets静态资源加载问题
+    origin: '//localhost:8888'
   }
 })
